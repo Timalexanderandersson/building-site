2dc83d8088e7ba64651eba7c67942b3fd4d07387
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-04-10 11:07:28 +0200

    about us

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import styles from '../styles/aboutus.module.css'; // CSS-modul för stilar
+import styles from '../styles/aboutus.module.css';
+import face from '../assets/new.jpg';
+import { Link } from 'react-router-dom'; // Exempel på en bildimport (ändra denna om du har andra bilder)
 
 const AboutUs = () => {
   return (
     <div className={styles.aboutUsContainer}>
+      <div className={styles.aboutUsContainerinner}>
+
       <div className={styles.header}>
         <h1>Om Oss</h1>
         <p>Vi är ett passionerat hantverkarteam som erbjuder pålitliga och professionella tjänster för alla dina bygg- och renoveringsbehov.</p>
@@ -20,18 +24,18 @@ const AboutUs = () => {
         <h2>Vårt Team</h2>
         <div className={styles.teamMembers}>
           <div className={styles.teamMember}>
-            <img src="https://via.placeholder.com/150" alt="Team Member" />
-            <h3>Anders Johansson</h3>
-            <p>Byggmästare med över 15 års erfarenhet inom branschen.</p>
+            <img src={face} alt="Anders Johansson" /> {/* Här används den importerade bilden */}
+            <h3>Person</h3>
+            <p>Byggmästare med över x antal års erfarenhet inom branschen.</p>
           </div>
           <div className={styles.teamMember}>
-            <img src="https://via.placeholder.com/150" alt="Team Member" />
-            <h3>Eva Svensson</h3>
-            <p>Expert på inredning och design för hållbara hem.</p>
+            <img src={face} alt="Eva" />
+            <h3>Person</h3>
+            <p>Snickare och allsidig hantverkare med fokus på detaljer.</p>
           </div>
           <div className={styles.teamMember}>
-            <img src="https://via.placeholder.com/150" alt="Team Member" />
-            <h3>Johan Eriksson</h3>
+            <img src={face} alt="Johan" />
+            <h3>Person</h3>
             <p>Snickare och allsidig hantverkare med fokus på detaljer.</p>
           </div>
         </div>
@@ -40,8 +44,11 @@ const AboutUs = () => {
       <section className={styles.contactSection}>
         <h2>Kontakta Oss</h2>
         <p>Har du frågor eller vill du boka ett möte? Tveka inte att höra av dig till oss!</p>
-        <button className={styles.contactButton}>Kontakt</button>
+        <Link to="/kontakt" className={styles.contactButton}>Kontakt</Link>
       </section>
+
+      </div>
+     
     </div>
   );
 };
